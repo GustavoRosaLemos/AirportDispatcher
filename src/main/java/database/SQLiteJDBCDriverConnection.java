552e3dbefe4e2ee5d629a65c07e5fabc45905c82
Gustavo <dev.gustavolemos@outlook.com>
2021-12-10 15:15:43 -0300

    Fixed database connection bug.

--- src/main/java/database/SQLiteJDBCDriverConnection.java
+++ src/main/java/database/SQLiteJDBCDriverConnection.java
@@ -6,23 +6,11 @@ import exception.DatabaseDisconnectException;
 import java.sql.*;
 
 public class SQLiteJDBCDriverConnection {
-    private static Connection connection;
     public static Connection connect() throws DatabaseConnectionException {
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:banco.db");
-            return connection;
+            return DriverManager.getConnection("jdbc:sqlite:banco.db");
         } catch (SQLException e) {
            throw new DatabaseConnectionException("Falha ao conectar banco de dados > " + e.getMessage());
-        }
-    }
-
-    public static boolean disconnect() throws DatabaseDisconnectException {
-        try {
-            connection.close();
-            connection = null;
-            return true;
-        } catch (SQLException e) {
-            throw new DatabaseDisconnectException("Falha ao desconectar banco de dados > " + e.getMessage());
         }
     }
 }
