dfa9e43a96e1a00a169048d200e59e629df6a1a8
Gustavo <dev.gustavolemos@outlook.com>
2022-02-21 16:17:06 -0300

    added interfaces

---
+++ src/main/java/interfaces/Comparator.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import lombok.Builder;
+import model.PackageHistory;
+
+import java.time.LocalDateTime;
+
+@Builder
+public class Comparator implements java.util.Comparator<PackageHistory> {
+    @Override
+    public int compare(PackageHistory p1, PackageHistory p2) {
+        LocalDateTime p1d = LocalDateTime.parse(p1.getCreatedAt());
+        LocalDateTime p2d = LocalDateTime.parse(p2.getCreatedAt());
+
+        if (p1d.isAfter(p2d)) {
+            return 1;
+        }
+        return 0;
+    }
+}
